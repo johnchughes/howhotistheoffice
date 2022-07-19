45cb648969e914fa33d026c22c3a7c98f0b306fe
John <j.c.hughes7@gmail.com>
2022-07-19 21:18:43 +0100

    Fixed a few warnings

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [colour, setColour] = useState('yellow');
   const [reading, setReading] = useState(null);
  
-  const colorScale = new ColorScale(1, 100, ["#2e59e6","#76cf29", "#d9d443", "#c74734"]);
+  
  
   const fetchReadingData = () => {
     fetch('https://fn-temps.azurewebsites.net/api/temps?code=1sZNGbf5LX9o6eil2yQcgTKFGUPWv4klDBtfRfJqzcwEAzFuu7KIcw==')
@@ -18,19 +18,19 @@ function App() {
   useEffect(() => {
     fetchReadingData();
     const interval = setInterval(fetchReadingData, 150000);
+
+    return () => {
+      clearInterval(interval);
+    }
   }, []);
 
   useEffect(() => {
     if(reading == null)return;
+    const colorScale = new ColorScale(1, 100, ["#2e59e6","#76cf29", "#d9d443", "#c74734"]);
     const colourIndex = (reading.temperature/30)*100
     const hexValue = colorScale.getColor(colourIndex).toHexString(); // returns "rgba(127,127,127, 0.5)"
     setColour(hexValue);
   }, [reading]);
-
-  const onNumberChanged = (event) => {
-   
-    setReading({ ...reading, temperature: event.target.value });
-  }
 
   if (reading == null) {
     return (
