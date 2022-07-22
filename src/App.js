5ea295fefdabc5c9936782115ef05cb9eb056488
John <j.c.hughes7@gmail.com>
2022-07-22 08:32:34 +0100

    fixed crappy colours, added time stamp

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.scss';
 import { useEffect, useState } from 'react';
-import *  as ColorScale from 'color-scales';
 import TodaysTemperatures from './TodaysTemperatures';
 
 function App() {
 
   const [colour, setColour] = useState('yellow');
   const [reading, setReading] = useState(null);
- 
+
   const fetchReadingData = () => {
     fetch('https://fn-temps.azurewebsites.net/api/temps?code=1sZNGbf5LX9o6eil2yQcgTKFGUPWv4klDBtfRfJqzcwEAzFuu7KIcw==')
       .then((response) => response.json())
@@ -17,17 +16,29 @@ function App() {
   useEffect(() => {
     fetchReadingData();
     const interval = setInterval(fetchReadingData, 150000);
-
     return () => {
       clearInterval(interval);
     }
   }, []);
 
+  const getColourForTemperature = (temperature) => {
+    if (temperature < 10) {
+      return "#6DC0D5";
+    }
+    else if (temperature >= 10 && temperature <= 25) {
+      return "#378D3A";
+    }
+    else if (temperature > 24 && temperature < 29) {
+      return "#E38A36";
+    }
+    else {
+      return "#AB3428"
+    }
+  }
+
   useEffect(() => {
-    if(reading == null)return;
-    const colorScale = new ColorScale(1, 100, ["#1d69b5","#1d69b5","#69b51d","#69b51d","#69b51d","#69b51d","#b5781d","#b5361d"]);
-    const colourIndex = (reading.temperature/30)*100
-    const hexValue = colorScale.getColor(colourIndex).toHexString(); // returns "rgba(127,127,127, 0.5)"
+    if (reading == null) return;
+    const hexValue = getColourForTemperature(reading.temperature);
     setColour(hexValue);
   }, [reading]);
 
@@ -37,16 +48,30 @@ function App() {
     );
   }
 
+  // const onChange = (event) => {
+  //   setReading({...reading, temperature: event.target.value});
+  // }
+
+  const FormatTimeStamp = (timestamp) => {
+    const now = new Date(timestamp);
+    now.setHours(now.getHours() - 1);
+    return now.toLocaleString();
+  }
+
   return (
     <div className='App'>
+      {/* <input type="number" onChange={onChange} /> */}
       <section className='climate' >
         <div className='title'>
           <h1>&nbsp;</h1>
         </div>
-        <div className='temperature' style={{ backgroundColor: colour }}>
-          <h1>{Math.round(reading.temperature * 10) / 10}&#8451;</h1>
+        <div className='temp-wrapper'>
+          <div className='temperature' style={{ backgroundColor: colour }}>
+            <h1>{Math.round(reading.temperature * 10) / 10}&#8451;</h1>
+            <span>{FormatTimeStamp(reading.temperatureTimeStamp)}</span>
+          </div>
         </div>
-        <TodaysTemperatures />
+        <TodaysTemperatures onColourPick={getColourForTemperature} />
       </section>
     </div>
   );
--- src/TodaysTemperatures.js
+++ src/TodaysTemperatures.js
@@ -1,25 +1,16 @@
 import { useEffect, useState } from "react";
-import *  as ColorScale from 'color-scales';
 import useWindowDimensions from "./hooks/useWindowDimensions";
 
-const TodaysTemperatures = () => {
+const TodaysTemperatures = ({onColourPick}) => {
 
     const [data, setData] = useState(null);
     const { width } = useWindowDimensions();
 
-    const getColourForTemperature = (TemperatureChart) => {
-        const colorScale = new ColorScale(1, 100, ["#1d69b5","#1d69b5","#69b51d","#69b51d","#69b51d","#69b51d","#b5781d","#b5361d"]);
-        const colourIndex = (TemperatureChart/30)*100
-        const hexValue = colorScale.getColor(colourIndex).toHexString(); // returns "rgba(127,127,127, 0.5)"
-        return hexValue;
-      }
-    
 
     useEffect(() => {
 
         const today = new Date();
         const dateString = today.getFullYear() + "-" + ("0" + (today.getMonth() + 1)).slice(-2) + "-" + today.getDate();
-        //const dateString = "2022-07-20";
         fetch('https://fn-temps.azurewebsites.net/api/TempsForDay?code=YxXpJgCmX6bnkVNXWN78CtErhAcb8I_4_6btELeCh1iYAzFuYnycaQ==&date='+dateString)
             .then(response => response.json())
             .then(result => setData(result));
@@ -38,7 +29,8 @@ const TodaysTemperatures = () => {
     const DayBars = data.map((x,i) => {
         const liHeight = x.temperature +"vh";
         const liWidth = ((width/24)-2) + "px";
-        return <li key={i} style={{height: liHeight, width: liWidth, backgroundColor: getColourForTemperature(x.temperature)}}></li>
+        const colour = onColourPick(x.temperature);
+        return <li key={i} style={{height: liHeight, width: liWidth, backgroundColor: colour}}></li>
       });
     
 
