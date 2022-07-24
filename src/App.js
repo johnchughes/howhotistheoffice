277fea1976181527791af9b4cb0ad2d3395725cb
John <j.c.hughes7@gmail.com>
2022-07-24 23:40:27 +0100

    improved error handling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import './App.scss';
 import { useEffect, useState } from 'react';
 import TodaysTemperatures from './TodaysTemperatures';
 import Weather from './Weather';
+import Temperature from './Temperature';
 
 function App() {
 
   const [colour, setColour] = useState('yellow');
   const [reading, setReading] = useState(null);
+  const [error, setError] = useState(null);
 
   const fetchReadingData = () => {
     fetch('https://fn-temps.azurewebsites.net/api/temps?code=1sZNGbf5LX9o6eil2yQcgTKFGUPWv4klDBtfRfJqzcwEAzFuu7KIcw==')
       .then((response) => response.json())
-      .then((data) => setReading(data));
+      .then((data) => setReading(data))
+      .catch(err => {
+        setError(err);
+      })
   }
 
   useEffect(() => {
@@ -43,37 +48,43 @@ function App() {
     setColour(hexValue);
   }, [reading]);
 
-  if (reading == null) {
+  if(error != null) {
     return (
-      <div>its friday yeah!</div>
+      <main>
+        <section className='App' >
+        <div className='temp-wrapper'>
+          <div className='temperature' style={{ backgroundColor: "black" }}>
+            <h1>Fuck</h1>
+            <p>Something went wrong ... </p>
+          </div>
+        </div>
+      </section>
+      </main >
     );
   }
 
-  // const onChange = (event) => {
-  //   setReading({...reading, temperature: event.target.value});
-  // }
-
-  const FormatTimeStamp = (timestamp) => {
-    const now = new Date(timestamp);
-    now.setHours(now.getHours() - 1);
-    return now.toLocaleString();
+  if (reading == null) {
+    return (
+      <main>
+        <section className='App' >
+        <div className='temp-wrapper'>
+          <div className='temperature' style={{ backgroundColor: "pink" }}>
+            <h1>Loading .... </h1>
+          </div>
+        </div>
+      </section>
+      </main >
+    );
   }
 
   return (
     <main>
-      {/* <input type="number" onChange={onChange} /> */}
       <section className='App' >
-          <Weather></Weather>
-        <div className='temp-wrapper'>
-          <div className='temperature' style={{ backgroundColor: colour }}>
-            <h2>current office temp</h2>
-            <h1>{Math.round(reading.temperature * 10) / 10}<sup>&#8451;</sup></h1>
-            <p>{FormatTimeStamp(reading.temperatureTimeStamp)}</p>
-          </div>
-        </div>
+        <Weather></Weather>
+        <Temperature reading={reading} colour={colour}></Temperature>
         <TodaysTemperatures onColourPick={getColourForTemperature} />
       </section>
-      </main>
+    </main>
   );
 }
 
